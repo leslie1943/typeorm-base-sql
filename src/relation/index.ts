df6a1ce8e2cffdd02020068f5425436cb1c7f4fa
leslie <leslie43@sina.com>
2021-10-11 07:36:08 +0800

    complicated query buider

--- src/relation/index.ts
+++ src/relation/index.ts
@@ -2,10 +2,12 @@ import { photo_OneToOne_meta } from "./photo.relation";
 import { photo_Both_OneToOne_meta } from "./photo.both-relation";
 import { photo_createQueryBuilder_OneToOne_meta } from "./photo.query-builder";
 import { photo_many_2_many_album } from "./photo.manytomany";
+import { photo_query_builder_complicated } from "./photo.query-builder-complicated";
 
 export {
   photo_OneToOne_meta,
   photo_Both_OneToOne_meta,
   photo_createQueryBuilder_OneToOne_meta,
   photo_many_2_many_album,
+  photo_query_builder_complicated,
 };
