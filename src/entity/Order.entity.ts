92edf4b27ac07eb88acbc95980abfff5fc6bc08a
leslie <leslie43@sina.com>
2021-10-19 09:16:41 +0800

    understanding for manytoone

--- src/entity/Order.entity.ts
+++ src/entity/Order.entity.ts
@@ -15,7 +15,11 @@ export class Order {
   @Column()
   orderName: string;
 
-  @ManyToOne(() => User, (user) => user.order)
-  @JoinColumn()
-  user: User;
+  /**
+   * 多对一 / 一对多的关系, 拥有方总是多对一的.
+   * 这意味着使用 @ManyToOne 的类将存储相关对象(反向关系的)的id(User中的id)
+   * 自动生成反向关系的id 无论是否使用 @JounColumn
+   */
+  @ManyToOne(() => User, (user) => user.orders)
+  user: User; // 生成 userId 列
 }
